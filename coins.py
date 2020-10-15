eafc7f4ce90b395a9f6973608b38c880a9324296
Jianying Sheng <j6sheng@uwaterloo.ca>
2020-10-14 22:51:19 -0400

    Adding test for dime quarter loonie and toonie

--- coins.py
+++ coins.py
@@ -20,3 +20,32 @@ class Nickel(Coin):
     """
     value: int = 5
     label: str = "5"+u'\xa2'
+
+@dataclass
+class Dime(Coin):
+    """
+    child class extended from coin
+    """
+    value: int = 10
+    label: str = "10"+u'\xa2'
+@dataclass
+class Quarter(Coin):
+    """
+    child class extended from coin
+    """
+    value: int = 25
+    label: str = "25"+u'\xa2'
+@dataclass
+class Loonie(Coin):
+    """
+    child class extended from coin
+    """
+    value: int = 100
+    label: str = '$1'
+@dataclass
+class Toonie(Coin):
+    """
+    child class extended from coin
+    """
+    value: int = 200
+    label: str = '$2'
--- tests/test_coins.py
+++ tests/test_coins.py
@@ -17,3 +17,58 @@ def test_nickel_str_label():
 
     coin = coins.Nickel()
     assert str(coin) == '5¢'
+
+def test_dime_value_10():
+    """
+    test child class Dime of Coin value is 10
+    """
+    coin = coins.Dime()
+    assert coin.value == 10
+
+def test_dime_str_label():
+    """
+    test dime child class of Coin label is 10¢
+    """
+    coin = coins.Dime()
+    assert str(coin) == '10¢'
+def test_quarter_value_25():
+    """
+    test child class quarter of Coin value is 25
+    """
+    coin = coins.Quarter()
+    assert coin.value == 25
+
+def test_quarter_str_label():
+    """
+    test quarter child class of Coin label is 25¢
+    """
+    coin = coins.Quarter()
+    assert str(coin) == '25¢'
+
+def test_loonie_value_100():
+    """
+    test child class loonie of Coin value is 100
+    """
+    coin = coins.Loonie()
+    assert coin.value == 100
+
+def test_loonie_str_label():
+    """
+    test loonie child class of Coin label is $1
+    """
+    coin = coins.Loonie()
+    assert str(coin) == '$1'
+
+def test_toonie_value_200():
+    """
+    test child class toonie of Coin value is 200
+    """
+    coin = coins.Toonie()
+    assert coin.value == 200
+
+def test_toonie_str_label():
+    """
+    test  toonie child class of Coin label is $2
+    """
+    coin = coins.Toonie()
+    assert str(coin) == '$2'
